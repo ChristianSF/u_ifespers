e1aba24c0c8e68db4d6190b2309cf115dc7c2cd0
leonardo <leonardo.costa@unifesp.br>
2023-08-19 21:40:46 -0300

    feat: add home component

--- quero_futuro/src/app/app.module.ts
+++ quero_futuro/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './features/home/home.module';
 import { ChatModule } from './features/chat/chat.module';
+import { ChatComponent } from './shared/components/chat/chat.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // ChatModule,
+    ChatModule,
     HomeModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

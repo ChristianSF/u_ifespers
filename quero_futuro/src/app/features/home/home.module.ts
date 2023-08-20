26ffeb97b215a864df0d9964574442372406de3c
Rafaela Uchôas <rafa.uchoas@gmail.com>
2023-08-20 01:59:19 -0300

    home

--- quero_futuro/src/app/features/home/home.module.ts
+++ quero_futuro/src/app/features/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+// import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}

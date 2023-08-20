ce547fee972217026b4d0cbb0c12f0143b1585f6
leonardo <leonardo.costa@unifesp.br>
2023-08-19 21:29:00 -0300

    feat: add navegacao entre as telas

--- quero_futuro/src/app/app-routing.module.ts
+++ quero_futuro/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ChatComponent } from './features/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'chat', component: ChatComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- quero_futuro/src/app/app.module.ts
+++ quero_futuro/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './features/home/home.module';
+import { ChatModule } from './features/chat/chat.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeModule } from './features/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    // ChatModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

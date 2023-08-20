168257463c5090c2f978219cb1eabdc7586d07e6
Christian Freitas <chris.sfreitas10@gmail.com>
2023-08-20 05:07:36 -0300

    feat new changes

--- api/app.py
+++ api/app.py
@@ -8,59 +8,63 @@ from sklearn.cluster import KMeans
 from flask import Flask, jsonify, request
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
-
 app = Flask(__name__)
 
-def get_credencials():
-    pass
+def get_credentials():
+    os.environ['OPENAI_API_KEY'] = "aa"
+
+    openai.organization = 'bb'
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def instancia_kmeans():
-    with open('model/models/kmeans_profissoes.pkl', 'rb') as arquivo:
+    with open('../model/models/kmeans_profissoes.pkl', 'rb') as arquivo:
         kmeans = pickle.load(arquivo)
 
+
+    print(1)
     return kmeans
 
 def define_modelo():
     embedding_model = "text-embedding-ada-002"
     embedding_encoding = "cl100k_base"
     max_tokens = 8000
-    
+
+    print(2)
     return embedding_model, embedding_encoding, max_tokens
 
-
-def get_embedding(string, embedding_model):
-
-    embedding = get_embedding(string, engine=embedding_model)
+def get_custom_embedding(string, embedding_model):
     
+    embedding = get_embedding(string, engine=embedding_model)
+    print(3)
     return embedding
 
 def get_matrix(embedding):
     matrix = np.array(embedding)
     matrix = matrix.reshape(1, -1)
 
+    print(4)
     return matrix
 
 def get_predict(matrix, kmeans):
-
     predict = kmeans.predict(matrix)
-
+    print(5)
     return int(predict[0])
 
 def get_data():
     df = pd.read_csv("../data/Model_embedding_Profissoes.csv")
-
     df = df[['nome_curso', 'descricao', 'cursos', 'Cluster']]
 
+    print(6)
     return df
 
 def recomenda(string):
-    get_credencials()
+    get_credentials()
 
     kmeans = instancia_kmeans()
 
     embedding_model, embedding_encoding, max_tokens = define_modelo()
 
-    embedding = get_embedding(string, embedding_model)
+    embedding = get_custom_embedding(string, embedding_model)
 
     matrix = get_matrix(embedding)
 
@@ -72,21 +76,19 @@ def recomenda(string):
 
     return new_df
 
-
 @app.route('/descricao', methods=['POST'])
 def process_array():
     try:
-        string = request.get_json()
-        if not isinstance(string, list):
-            return jsonify({'error': 'Invalid input. Expected an array.'}), 400
+        data = request.get_json()
         
-        result = recomenda(string)
+        print(data)
 
-        return jsonify({'result': result}), 200
+        result = recomenda(data)
+
+        return result
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
 
 if __name__ == '__main__':
     app.run()
